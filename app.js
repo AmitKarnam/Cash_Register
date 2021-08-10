3e0286995cfdb503c1ea2e4a29681a8d82643eb2
AmitKarnam <amitkarnam01@gmail.com>
2021-08-10 18:16:31 +0530

    refactored the js script and modularised it

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 const billAmount = document.querySelector("#bill-amount");
-
-const nextBtn  =  document.querySelector("#nextBtn");
-nextBtn.addEventListener('click',() => {
-
-/* Number type is like double in Java or C# */
-    if(Number(billAmount.value) > 0){
-        console.log("Next button clicked.");
-        console.log("Bill amount is equal to : " + Number(billAmount.value))
-    }
-
-    else{
-        alert("Bill amount should be greater than 0.");
-    }
-})
-
 const cashGiven = document.querySelector("#cash-given");
-
 const checkBtn = document.querySelector("#checkBtn");
-checkBtn.addEventListener('click', () => {
+const errorMsg = document.querySelector("#error-message");
+
+checkBtn.addEventListener('click',  function validateBillandCashAmount() {
+
+    hideMessage();
+
+    // The JavaScript Number type is a double-precision 64-bit binary format IEEE 754 value, like double in Java.
+    if(Number(billAmount.value) > 0){
+        console.log("Bill amount is equal to : " + Number(billAmount.value))
+
+        if(Number(cashGiven.value) >= Number(billAmount.value)){
+            console.log("Cash given is : " + Number(cashGiven.value))
+        }
     
-    if(Number(cashGiven.value) >= Number(billAmount.value)){
-        console.log("Check button pressed!");
-        console.log("Cash given is : " + Number(cashGiven.value))
+        else{
+            showMessage("Please provide sufficcient amount.");
+        }
     }
 
     else{
-        alert("Please provide sufficcient amount.")
+        showMessage("Bill amount should be positive and greater than 0");
     }
-
 })
+
+function hideMessage(){
+    errorMsg.style.display = "none";
+}
+
+function showMessage(msg){
+    errorMsg.style.display = "block";
+    errorMsg.style.color = "red";
+    errorMsg.innerText = msg;
+}
 
 
 
